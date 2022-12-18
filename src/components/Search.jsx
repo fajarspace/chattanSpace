7701575894ac9d725670ec3c3a48cb4cdba263cd
fajarspace <fajar.agngn@gmail.com>
2022-12-18 14:33:04 +0700

    add client chat

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder='Cari orang...' />
+      </div>
+      <div className="userChat">
+        <img src="https://avatars.githubusercontent.com/u/52228231?v=4" alt="" />
+        <div className="userChatInfo">
+          <span>Fajar</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
