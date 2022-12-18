5d9552f640d0c34f98ba8695e136d54110592687
fajarspace <fajar.agngn@gmail.com>
2022-12-18 11:43:04 +0700

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,11 +5,13 @@ const Register = () => {
     <div className='formContainer'>
       <div className="formWrapper">
         <span className="logo">chattanSpace</span>
+        <span className="title">Register</span>
         <form action="">
           <input type="text" className="text" placeholder='display name' />
           <input type="email" className="email" placeholder='email' />
           <input type="password" className="password" placeholder='password' />
           <input type="file" className="file" />
+          <button>Daftar</button>
         </form>
       </div>
     </div>
