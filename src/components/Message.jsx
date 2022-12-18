4193e89c878a245fce4bd6dc2a4c238858369247
fajarspace <fajar.agngn@gmail.com>
2022-12-18 15:16:13 +0700

    add client chat

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div >
-      ha;o
+    <div className="message">
+      <div className="messageInfo">
+        <img src="" alt="" />
+        <span>Baru saja</span>
+      </div>
+      <div className="messageContent">
+        <p>Halo</p>
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
