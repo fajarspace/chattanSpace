9f7a936c22d3e592ee0f97d43b79f1a9e495a931
Fajar Agung <fajar.agngn@gmail.com>
2022-12-20 14:10:31 +0700

    config register using firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Home from "./pages/Home";
-// import Register from "./pages/Register";
+import Register from "./pages/Register";
 // import Login from "./pages/Login";
 import { } from "./style.scss";
 
 const App = () => {
   return (
-    <Home />
+    <Register />
   )
 }
 
