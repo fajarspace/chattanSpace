b423134fd3a56ea08253ece1bf5ab4cf9a21e5ac
Fajar Agung <fajar.agngn@gmail.com>
2022-12-20 19:38:55 +0700

    success login!

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -4,17 +4,27 @@ import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
 
 const Chats = () => {
+  const history = useHistory();
+  const handleLogout = async () => {
+    await auth.signOut();
+    history.push('/');
+  }
   return (
     <div className="chats-page">
       <div className="nav-bar">
         <div className="logo-tab">
           chattanSpace
         </div>
-        <div className="logout-tab">
+        <div onClick={handleLogout} className="logout-tab">
           Logout
         </div>
       </div>
-      <ChatEngine />
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectId="0c128a3a-9b56-499a-b713-b48c705bdc6a"
+        userName="."
+        userSecret="."
+      />
     </div>
   )
 }
