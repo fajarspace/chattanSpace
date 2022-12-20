a57448cd451b67399f19b864c87e8fe8571f042c
Fajar Agung <fajar.agngn@gmail.com>
2022-12-20 18:54:54 +0700

    Add auth google & facebook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Login from "./Login";
+import Login from "./components/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 // import { AuthProvider } from "../contexts/AuthContext"
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { GoogleCircleFilled, FacebookFilled } from "@ant-design/icons";
+import "firebase/app";
+import { auth } from "../firebase";
+import firebase from "firebase/app";
 
 const Login = () => {
   return (
     <div id='login-page' >
       <div id="login-card">
         <h2>Chattan Space</h2>
-        <div className="login-button google">
+        <div className="login-button google" onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
           <GoogleCircleFilled /> Masuk dengan Google
         </div><br /><br />
-        <div className="login-button facebook">
+        <div className="login-button facebook" onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
           <FacebookFilled /> Masuk dengan Facebook
         </div>
       </div>
